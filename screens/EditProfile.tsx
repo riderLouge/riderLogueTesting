6985dec30b5062dfa61789ff2c0b7663ca1886f0
riderLogue <riderlogue.official@gmail.com>
2023-04-06 12:44:09 +0530

    edit profile updated

--- screens/EditProfile.tsx
+++ screens/EditProfile.tsx
@@ -17,7 +17,6 @@ var {width,height} = Dimensions.get('window')
 const EditProfile = () => {
 
     const[Pic,SetPic]=React.useState('');
-    const[CoverPic,SetCoverPic]=React.useState('');
     const[ProfileName,SetProfileName]=React.useState('');
     const[FirstName,SetFirstName]=React.useState('');
     const[LastName,SetLastName]=React.useState('');
@@ -29,27 +28,29 @@ const EditProfile = () => {
     const Navigation = useNavigation();
     const db = firebase.firestore();
     const [count,SetCount]=React.useState(0);
+    const uid = firebase.auth().currentUser?.uid
     
-    var proname = "";
     
-    db.collection('posts').get().then((snapshot) => {
-        snapshot.docs.forEach(doc=>{
-        renderData(doc);
+    db.collection('users').get().then((snapshot) => {
+        snapshot.forEach(doc=>{
+            if(doc.id === uid){
+                renderData(doc);
+            }            
         })
     })
     function renderData(doc){
         if(count==0){
-            SetPic(doc.data().profilePic);
-            SetCoverPic(doc.data().coverPic);
-            SetProfileName(doc.data().proname);
-            SetFirstName(doc.data().firstName);
-            SetLastName(doc.data().lastName);
-            SetDOB(doc.data().dateOfBirth);
-            SetPhoneNumber(doc.data().phoneNumber);
-            SetEmail(doc.data().email);
-            SetCountry(doc.data().country);
-            SetCity(doc.data().city);
+            SetPic(doc.data().ProfilePic);
+            SetProfileName(doc.data().ProfileName);
+            SetFirstName(doc.data().FirstName);
+            SetLastName(doc.data().LastName);
+            SetDOB(doc.data().DOB);
+            SetPhoneNumber(doc.data().PhoneNumber);
+            SetEmail(doc.data().Email);
+            SetCountry(doc.data().Country);
+            SetCity(doc.data().City);
         }
+        console.log(Pic)
     }
     async function profilePic(){
         try {
@@ -69,36 +70,20 @@ const EditProfile = () => {
             }  
         }
     }
-    async function coverPic(){
-        try {
-            const file = await DocumentPicker.pickSingle({
-                type: [DocumentPicker.types.images],
-            });
-            console.log(file.uri)
-            const path = file.uri;
-            const result = await RNFetchBlob.fs.readFile(path, "base64")
-            SetCoverPic(result);
-        } catch (err) {
-            if(DocumentPicker.isCancel(err)){
-
-            }
-            else {
-                throw err;
-            }  
-        }
-    }
+    
     function save(){
-        firestore().collection('posts').add({
-            profilePic: Pic,
-            coverPic: CoverPic,
-            proname: ProfileName,
-            firstName: FirstName,
-            lastName: LastName,
-            dateOfBirth: DOB,
-            phoneNumber: PhoneNumber,
-            email: Email,
-            country: Country,
-            city: City,
+        firestore().collection('users')
+        .doc(firebase.auth().currentUser?.uid)
+        .update({
+            ProfilePic: Pic,
+            ProfileName: ProfileName,
+            FirstName: FirstName,
+            LastName: LastName,
+            DOB: DOB,
+            PhoneNumber: PhoneNumber,
+            Email: Email,
+            Country: Country,
+            City: City,
         })
         .then(() => {
             console.log('Post Added!');
@@ -127,44 +112,29 @@ state ={
                     <Button  
                         mode='contained' color="#121212" title='save profile'
                         type="outline"
-                        buttonStyle={{borderColor:'white'}}
+                        buttonStyle={{borderColor:'white', marginTop:8}}
                         onPress={() => {save(); Navigation.push("ProfileScreen")}}
                         titleStyle={{fontSize:width/40, color:'white'}}>
                     </Button>
                 </View>
             </View>
             <View style={{flex:10}}>
-                <View style={{flexDirection:'row', borderWidth:.5, borderColor:'grey', height: width/2}}>
-                    <ImageBackground
-                        style={{flex:1}}
-                        source={{uri:'data:image/jpg;base64,'+CoverPic}}>
-                        <View>
-                            <View style={{justifyContent: 'center', paddingLeft:'3%'}}>
-                                <View style={{backgroundColor:'black', width: width/2.9, borderRadius:120, height: width/2.9 ,marginTop: width/12}}>
-                                    <View style={{zIndex: 1}}>
-                                        <Avatar.Image
-                                        style={{backgroundColor:"#121212", marginLeft:'1.5%', marginTop:'2%'}}
-                                        size={width/3}
-                                        source={{uri:'data:image/jpg;base64,'+Pic}} />
-                                        <Icon2 name="camera-sharp"
-                                            size={width/13}
-                                            style={styles.profileiconContainer}
-                                            onPress={profilePic}>
-                                        </Icon2>
-                                    </View>
-                                </View>
-                            </View>
-                            <View style={[styles.centerContent, {marginTop:'10%', marginLeft:'5%', flexDirection:'row'}]}>
-                                <View style={styles.removeImage}>
-                                    <Icon2 name="camera-sharp"
-                                        size={width/13}
-                                        style={styles.iconContainer}
-                                        onPress={coverPic}>
-                                    </Icon2>
-                                </View>
+                <View style={{flexDirection:'row', height: width/2, alignItems:'center', justifyContent:'center'}}>                
+                    <View>
+                        <View style={{justifyContent: 'center', paddingLeft:'3%'}}>
+                            <View style={{backgroundColor:'white', width: width/2.9, borderRadius:120, height: width/2.9}}>
+                                <Avatar.Image
+                                    style={{backgroundColor:"grey", marginLeft:'1.5%', marginTop:'2%'}}
+                                    size={width/3}
+                                    source={{uri:'data:image/jpg;base64,'+Pic}} />
+                                <Icon2 name="camera-sharp"
+                                    size={width/13}
+                                    style={styles.profileiconContainer}
+                                    onPress={profilePic}>
+                                </Icon2>
                             </View>
                         </View>
-                    </ImageBackground>
+                    </View>
                 </View>
                 <View style={{flexDirection:'row', justifyContent:'center', alignItems:'center', paddingTop:'4%'}}>
                     <Icon name="ninja" size={width/12} color='white'></Icon>
@@ -175,7 +145,6 @@ state ={
                         defaultValue={ProfileName}
                         onPressIn={()=>{
                             SetCount(1)
-                            SetProfileName('')
                             console.log(count)
                             console.log(ProfileName)}}
                         style={[styles.textInput , {width:'70%', marginLeft: '3%', marginRight:'7%'}]}/>
@@ -189,7 +158,6 @@ state ={
                         defaultValue={FirstName}
                         onPressIn={()=>{
                             SetCount(1)
-                            SetFirstName('')
                             console.log(count)
                             console.log(FirstName)}}
                         style={[styles.textInput , {width:'70%', marginLeft: '3%', marginRight:'7%'}]}></TextInput>
@@ -203,7 +171,6 @@ state ={
                         defaultValue={LastName}
                         onPressIn={()=>{
                             SetCount(1)
-                            SetLastName('')
                             console.log(count)
                             console.log(LastName)}}
                         style={[styles.textInput , {width:'70%', marginLeft: '3%', marginRight:'7%'}]}></TextInput>
@@ -217,7 +184,6 @@ state ={
                         defaultValue={DOB}
                         onPressIn={()=>{
                             SetCount(1)
-                            SetDOB('')
                             console.log(count)
                             console.log(DOB)}}
                         style={[styles.textInput , {width:'70%', marginLeft: '3%', marginRight:'7%'}]}></TextInput>
@@ -231,7 +197,6 @@ state ={
                         defaultValue={PhoneNumber}
                         onPressIn={()=>{
                             SetCount(1)
-                            SetPhoneNumber('')
                             console.log(count)
                             console.log(PhoneNumber)}}
                         style={[styles.textInput , {width:'70%', marginLeft: '3%', marginRight:'7%'}]}></TextInput>
@@ -245,7 +210,6 @@ state ={
                         defaultValue={Email}
                         onPressIn={()=>{
                             SetCount(1)
-                            SetEmail('')
                             console.log(count)
                             console.log(Email)}}
                         style={[styles.textInput , {width:'70%', marginLeft: '3%', marginRight:'7%'}]}></TextInput>
@@ -259,7 +223,6 @@ state ={
                         defaultValue={Country}
                         onPressIn={()=>{
                             SetCount(1)
-                            SetCountry('')
                             console.log(count)
                             console.log(Country)}}
                         style={[styles.textInput , {width:'70%', marginLeft: '3%', marginRight:'7%'}]}></TextInput>
@@ -273,7 +236,6 @@ state ={
                         defaultValue={City}
                         onPressIn={()=>{
                             SetCount(1)
-                            SetCity('')
                             console.log(count)
                             console.log(City)}}
                         style={[styles.textInput , {width:'70%', marginLeft: '3%', marginRight:'7%'}]}></TextInput>
